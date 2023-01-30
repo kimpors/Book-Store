a00b540d01927ea4cb2a9d26b3da9538e11106d2
kimpors <mikita_korshak@ukr.net>
2023-01-30 20:35:42 +0200

    Вирішив почати все спочатку

--- BookConnection.session.sql
+++
@@ -1,9 +0,0 @@
-INSERT INTO Books
-VALUES (
-        1,
-        "SomeBook",
-        120,
-        "SomeAuthor",
-        "SomeDescription",
-        "2020-12-31"
-    );
--- Program.cs
+++ Program.cs
@@ -1,26 +1,9 @@
-using Microsoft.EntityFrameworkCore;
-using Pomelo.EntityFrameworkCore.MySql;
-using Book_Store.Data;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDistributedMemoryCache();
-
-builder.Services.AddSession(options =>
-{
-    options.IdleTimeout = TimeSpan.FromSeconds(20);
-    options.Cookie.HttpOnly = true;
-    options.Cookie.IsEssential = true;
-});
-
-string connection = builder.Configuration.GetConnectionString("Default");
-builder.Services.AddDbContext<BookDbContext>(options =>
-    options.UseMySql(connection, ServerVersion.AutoDetect(connection)));
 
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -37,10 +20,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.UseSession();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Book}/{action=Index}/{id?}/{quantity?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
